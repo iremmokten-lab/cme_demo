fef5043dd97c6af3d98863a8c46e916e2e63148d
iremmokten-lab <iremmokten@gmail.com>
2026-02-21 22:48:15 +0300

    Update session.py

--- src/db/session.py
+++ src/db/session.py
@@ -5,10 +5,3 @@ DB_URL = "sqlite:///app.db"
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
