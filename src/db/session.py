4e66cafc95176ce87cce2614c94cde5e3ea2849b
iremmokten-lab <iremmokten@gmail.com>
2026-02-22 12:11:52 +0300

    Update session.py

--- src/db/session.py
+++ src/db/session.py
@@ -3,5 +3,15 @@ from sqlalchemy.orm import sessionmaker
 
 DB_URL = "sqlite:///app.db"
 
-engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+engine = create_engine(
+    DB_URL,
+    connect_args={"check_same_thread": False},
+    future=True,
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    future=True,
+)
