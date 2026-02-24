f628a5c4449d5182320c4bb256ed9b3c46bcc883
iremmokten-lab <iremmokten@gmail.com>
2026-02-24 22:28:40 +0300

    Delete carbon_platform directory

--- carbon_platform/services/api/jobs/queue.py
+++
@@ -1,8 +0,0 @@
-from redis import Redis
-from rq import Queue
-from services.api.core.config import settings
-
-
-redis_conn = Redis.from_url(settings.REDIS_URL)
-
-queue = Queue("carbon_jobs", connection=redis_conn)
