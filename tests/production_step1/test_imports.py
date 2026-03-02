4ddb563430f92d83b276e7928ebec7fb5ec2f700
iremmokten-lab <iremmokten@gmail.com>
2026-03-02 17:18:36 +0300

    Create test_imports.py

---
+++ tests/production_step1/test_imports.py
@@ -0,0 +1,5 @@
+def test_imports_step1():
+    import src.db.production_step1_models  # noqa: F401
+    import src.services.cbam_portal_client  # noqa: F401
+    import src.services.data_governance  # noqa: F401
+    import src.services.verifier_workspace  # noqa: F401
